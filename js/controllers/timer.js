ae7e15254b183ecc8d07c4d5a2c886ab27810cac
Boo-urns <coreyaburns@gmail.com>
2013-07-25 16:21:57 -0500

    Almost done updating timer

--- js/controllers/timer.js
+++ js/controllers/timer.js
@@ -4,9 +4,21 @@ app.controller( 'TimerCtrl', function CountdownCtrl($scope,$timeout) {
     $scope.minutes  = 0,
     $scope.seconds  = 0,
     $scope.pauseBtn = 1;
-    var countdown;
+
+    $scope.startTimestamp = 0;
+    var countdown, diff;
+
+    // will cleanup this var later
+    var current;
 
     $scope.start = function() {
+    	if($scope.startTimestamp == 0) {
+    		$scope.startTimestamp = new Date().getTime();
+    	} else {
+    		diff = (new Date().getTime() - $scope.startTimestamp) / 1000;
+    		console.log(diff);
+    	}
+
     	$scope.pauseBtn = 1,
     	$scope.started  = 1;
     	
@@ -30,12 +42,12 @@ app.controller( 'TimerCtrl', function CountdownCtrl($scope,$timeout) {
 	            }
 	        }        
 	
-	    	}, 1000);
+	    	}, (1000 - diff));
     	}
     }
       
 		$scope.pause = function() {
-			$scope.pauseBtn = 0,
+			$scope.pauseBtn = 0;
 			$scope.started  = 0;
 		  $timeout.cancel(countdown);
 		};
@@ -46,3 +58,28 @@ app.controller( 'TimerCtrl', function CountdownCtrl($scope,$timeout) {
 		}
      
 });
+
+
+/*
+var start = new Date().getTime();
+var time = 0,
+    elapsed = '0.0';
+
+var timer = 10; // 20 seconds to start w/
+var current;
+
+
+function countDown()
+{
+    var diff = (new Date().getTime() - start) / 1000;
+    current = timer - Math.round(diff);
+
+    document.title = current;
+
+    if(current > 0) {
+    window.setTimeout(countDown, (1000 - diff));
+  	}
+}
+window.setTimeout(countDown, 1000);
+
+*/
