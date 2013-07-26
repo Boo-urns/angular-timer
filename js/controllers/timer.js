7567d41081eac274453ca0cebdeeda68b9ff2294
Boo-urns <coreyaburns@gmail.com>
2013-07-26 15:56:45 -0500

    Refactoring Timer Code

--- js/controllers/timer.js
+++ js/controllers/timer.js
@@ -6,50 +6,50 @@ app.controller( 'TimerCtrl', function CountdownCtrl($scope,$timeout) {
     $scope.pauseBtn = 1;
 
     $scope.startTimestamp = 0;
-    var countdown, diff;
-
-    // will cleanup this var later
-    var current;
+    var diff, timerPromise;
 
     $scope.start = function() {
-    	if($scope.startTimestamp == 0) {
-    		$scope.startTimestamp = new Date().getTime();
-    	} else {
-    		diff = (new Date().getTime() - $scope.startTimestamp) / 1000;
-    		console.log(diff);
-    	}
+ 	
+	    	if($scope.startTimestamp == 0) {
+	    		$scope.startTimestamp = new Date().getTime();
+	    		$scope.pauseBtn = 1,
+	    		$scope.started  = 1;
+	    	} else {
+	    		diff = (new Date().getTime() - $scope.startTimestamp) / 1000;
+	    		console.log(diff);
+	    	}
+				
+ 			// Why is timerPromise up top instead of bottom?
+ 			// it won't cancel if it's at the bottom!!
+ 			timerPromise = $timeout($scope.start, 1000);
+  
 
-    	$scope.pauseBtn = 1,
-    	$scope.started  = 1;
-    	
-    	if($scope.minutes > 0 || ($scope.minutes == 0 && $scope.seconds >= 0)) {
-	    	countdown = $timeout(function() { 
-					$scope.seconds--;
-	
-	        if($scope.seconds > 0 && $scope.minutes == 0) {
-	        		$scope.start();
-	        
-	        } else if ($scope.seconds >= 0 && $scope.minutes > 0) {
-	        		$scope.start();
-	        }
-	        else {
-	        		if($scope.minutes > 0) {
-		        		$scope.minutes--;
-		            $scope.seconds = 59;
-		            $scope.start();
-	            } else {
-	            	$timeout.cancel(countdown);
-	            }
-	        }        
-	
-	    	}, (1000 - diff));
-    	}
+        if($scope.seconds > 0 && $scope.minutes >= 0) {
+        		$scope.seconds--;
+        
+        } else {
+        		if($scope.minutes > 0) {
+	        		$scope.minutes--;
+	            $scope.seconds = 59;
+
+            } else if($scope.seconds > 0) {
+            	$scope.seconds--;
+
+          	} else {
+            	$timeout.cancel(timerPromise);
+            	$scope.seconds = 0;
+            	console.log('countdown finished!');
+            	$scope.pause();
+            }
+        }        
+
     }
       
 		$scope.pause = function() {
 			$scope.pauseBtn = 0;
 			$scope.started  = 0;
-		  $timeout.cancel(countdown);
+			$scope.startTimestamp = 0;
+		  $timeout.cancel(timerPromise);
 		};
 		
 		$scope.resume = function() { 
@@ -58,28 +58,3 @@ app.controller( 'TimerCtrl', function CountdownCtrl($scope,$timeout) {
 		}
      
 });
-
-
-/*
-var start = new Date().getTime();
-var time = 0,
-    elapsed = '0.0';
-
-var timer = 10; // 20 seconds to start w/
-var current;
-
-
-function countDown()
-{
-    var diff = (new Date().getTime() - start) / 1000;
-    current = timer - Math.round(diff);
-
-    document.title = current;
-
-    if(current > 0) {
-    window.setTimeout(countDown, (1000 - diff));
-  	}
-}
-window.setTimeout(countDown, 1000);
-
-*/
